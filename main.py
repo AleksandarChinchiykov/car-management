ff5b6d67bc3186ce040ccd0013c607161e8fb7f4
AleksandarChinchiykov <alchinch@cisco.com>
2024-12-31 02:11:57 +0200

    Added garage router to main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.car_route import router as car_router
+from routes.garage_route import router as garage_router
 from database.models import Base
 from database.database import engine
 
@@ -16,5 +17,7 @@ app.add_middleware(
 )
 
 app.include_router(car_router)
+app.include_router(garage_router)
+
 
 Base.metadata.create_all(bind=engine)
